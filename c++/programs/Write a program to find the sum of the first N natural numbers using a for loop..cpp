84bc77c52e439da3915a6224c97ee03d45dc7615
Pranav1357sys <rawatpranav4730@gmail.com>
2025-04-08 11:17:50 +0530

    Update Write a program to find the sum of the first N natural numbers using a for loop..cpp

---
+++ c++/programs/Write a program to find the sum of the first N natural numbers using a for loop..cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n,sum;
+cout<<"Enter the value of N: ";
+cin>>n;
+for(int i=0,i<=N;i++){
+sum+=i;
+}
+cout<<"The sum is: "<<sum<<endl;
+}
